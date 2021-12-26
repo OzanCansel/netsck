58f4b5316adf743a993a54026bcc487e8e96b3c5
OzanCansel <ozancansel@tutanota.com>
2021-12-26 08:33:05 +0300

    generate-pdf target added which converts man pages to pdfs and merges them into netsck-js-api.pdf. Class names made bold in docs.

---
+++ script/generate-pdf.sh
@@ -0,0 +1,35 @@
+#!/bin/env bash
+
+ps2pdf_exist=$(which ps2pdf > /dev/null; echo $?)
+man_exist=$(which man > /dev/null; echo $?)
+
+if [ "${man_exist}" != "0" ] 
+then
+    echo "'man' should be installed on the system."
+    exit 1
+fi
+
+if [ "${ps2pdf_exist}" != "0" ] 
+then
+    echo "'ps2pdf' should be installed on the system."
+    exit 1
+fi
+
+man="/bin/env man"
+ps2pdf="/bin/env ps2pdf"
+pdf_files_dir=$(mktemp -d)
+
+for file in *.7
+do
+    echo "Processing" ${file}
+
+    ts_file=${pdf_files_dir}/${file//".7"/".ts"}
+    pdf_file=${pdf_files_dir}/${file//".7"/".pdf"}
+
+    ${man} -t ./${file} > ${ts_file}
+    ${ps2pdf} ${ts_file} ${pdf_file}
+done
+
+pdfunite ${pdf_files_dir}/*.pdf netsck-js-api.pdf
+echo "'netsck-js-api.pdf' generated."
+echo
